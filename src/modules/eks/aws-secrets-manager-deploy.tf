3ce001274b13569bff2e26962e2d54586ef3148c
Oriol Deiros <oriol.deiros@upc.edu>
2024-02-19 22:06:10 +0100

    Add argocd and ingress ingress to EKS

--- src/modules/eks/aws-secrets-manager-deploy.tf
+++
@@ -1,8 +0,0 @@
-resource "helm_release" "secrets-store-csi-driver" {
-  name = "secrets-store-csi-driver"
-
-  repository = "https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts"
-  chart      = "secrets-store-csi-driver"
-  version    = "1.4.1"
-  namespace  = "kube-system"
-}
