cfc18dc0487916ba046e6a60ba1f9a4b36704487
Oriol Deiros <oriol.deiros@upc.edu>
2024-02-23 10:27:31 +0100

    Update database name

--- src/modules/rds/variables.tf
+++ src/modules/rds/variables.tf
@@ -7,7 +7,7 @@ variable "identifier" {
 variable "db_name" {
   description = "The name of the database to create when the DB instance is created."
   type        = string
-  default     = "postgres"
+  default     = "petclinic"
 }
 
 variable "engine" {
