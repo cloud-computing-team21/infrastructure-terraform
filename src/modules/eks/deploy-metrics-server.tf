0ead115bf0c3c098d6ec2db0eec435d16186cbe1
Oriol Deiros <oriol.deiros@upc.edu>
2024-02-23 23:41:18 +0100

    Add metrics-server and rename deploy resources

---
+++ src/modules/eks/deploy-metrics-server.tf
@@ -0,0 +1,8 @@
+resource "helm_release" "metrics-server" {
+  name = "metrics-server"
+
+  repository = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart      = "metrics-server"
+  version    = "3.12.0"
+  namespace  = "kube-system"
+}
